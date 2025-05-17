71279085eaea76af22607b59e44a102da35fc5ac
Aditya-ps88 <prakashaditya113@gmail.com>
2025-05-17 19:58:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function downloadAlert() {
+  alert("This is a demo download. Actual file download coming soon!");
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  alert("Thanks for your message! We’ll get back to you soon.");
+  return false;
+}
